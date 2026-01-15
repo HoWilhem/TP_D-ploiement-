0c17b2df1a852f3e17f6a3b37b1023883751b60c
Wilhem <hao-xuan-wilhem@efrei.net>
2026-01-15 13:28:43 +0100

    3e commit

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,9 +1,21 @@
 const request = require('supertest');
 const app = require('../backend/app');
 
+let server;
+
+beforeAll((done) => {
+  // démarre le serveur sur un port temporaire avant les tests
+  server = app.listen(3001, done);
+});
+
+afterAll((done) => {
+  // ferme le serveur après les tests pour que Jest s'arrête correctement
+  server.close(done);
+});
+
 describe('API Destinations', () => {
   it('returns destinations list', async () => {
-    const res = await request(app).get('/api/destinations');
+    const res = await request(server).get('/api/destinations');
     expect(res.statusCode).toBe(200);
     expect(res.body.length).toBe(3);
   });
